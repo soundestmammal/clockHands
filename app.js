c04686283b37942c9ab3472f92e3953eed0673ae
soundestmammal <robert.checco@gmail.com>
2017-05-25 19:24:59 -0400

    Update js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("this can be seen");
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+function setDate() {
+	const now = new Date();
+	const seconds = now.getSeconds();
+	const secondsDegrees = ((seconds / 60) * 360) + 90;
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+	const mins = now.getMinutes();
+	const minutesDegrees = ((mins / 60) * 360) + 90;
+	minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+	const hours = now.getHours();
+	const hoursDegrees = ((hours / 12) * 360) + 90;
+	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+	console.log(seconds,"seconds");
+	console.log(mins,"minutes");
+	console.log(hours,"hours");
+
+}
+
+setInterval(setDate , 1000);
